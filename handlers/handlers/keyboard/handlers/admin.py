12d3b38d29f77c40c9e945bf166261097f874fa1
niyazmmm <murzabekkov23@gmail.com>
2023-05-01 18:53:03 +0300

    hw4

---
+++ handlers/handlers/keyboard/handlers/admin.py
@@ -0,0 +1,41 @@
+from database.bot_db import sql_command_all, sql_command_delete
+from config import bot, dp
+from aiogram import types
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram import Bot, Dispatcher
+
+
+async def delete_data(message: types.Message):
+    users = await sql_command_all()
+    for user in users:
+        await bot.send_message(message.from_user.id, f"id - {user['id']},\n"
+                                                     f"имя - {user['name']},\nнаправление - {user['direction']},\n"
+                                                     f"возраст - {user['age']}, \n "
+                                                     f"группа - {user['group']}",
+                               reply_markup=InlineKeyboardMarkup().add(
+                                   InlineKeyboardButton(f"Удалить {user[1]}", callback_data=f"delete {user[0]}")))
+
+
+async def delete_user(message: types.Message):
+    users = await sql_command_all()
+    for user in users:
+        await bot.send_message(message.from_user.id,
+                               f"id - {user[0]},name - {user[1]},dir - {user[2]}, "
+                               f"age - {user[3]}, group - {user[4]}",
+                               reply_markup=InlineKeyboardMarkup().add(
+                                   InlineKeyboardButton(f"Delete {user[1]}", callback_data=f"delete {user[0]}")
+                               ))
+
+
+async def complete_delete(call: types.CallbackQuery):
+    await sql_command_delete(int(call.data.replace('delete ', '')))
+    await call.answer(text="Стёрт с базы данных", show_alert=True)
+    await bot.delete_message(call.message.chat.id, call.message.message_id)
+
+
+def register_message_admin(dp: Dispatcher):
+    dp.register_message_handler(delete_user, commands=["del"])
+    dp.register_callback_query_handler(
+        complete_delete,
+        lambda call: call.data and call.data.startswith("delete ")
+    )
---
+++ handlers/handlers/keyboard/handlers/keyboards/client.py
@@ -0,0 +1,6 @@
+ one_time_keyboard=True
+).add(KeyboardButton('CANCEL'))
+direction_markup.add(direction_back, direction_front, direction_android).add(direction_uxui, direction_ios).add(
+    cancel_markup)
+    KeyboardButton("CANCEL"))
+submit_markup.add(submit_markup1, submit_markup2)
